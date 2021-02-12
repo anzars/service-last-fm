c637c9e035a45f491f6e8cd7e3295e8750bad3d6
anaik <Anzar.naik@tal.com.au>
2021-02-13 10:44:44 +1100

    fetching data from remote sources

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const LastFM = require('last-fm')
+const lastfm = new LastFM('fd88c46c00f820169c8d37a53f2bccb2', { userAgent: 'MyApp/1.0.0 (http://example.com)' })
+const axios = require('axios');
+
+axios.get('https://api.first.org/data/v1/countries')
+    .then(response => {
+        console.log(response.data[0].country);
+        
+       
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
+lastfm.geoTopArtists( {country: 'India'}, (err, data) => { 
+    if (err) console.error(err)
+    else console.log(data)
+});
+ 
+lastfm.artistTopTracks({name:'Arjit'}, (err, data) => {
+    if (err) console.error(err)
+    else console.log(data)
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
